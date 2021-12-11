8c6e75add1b956e4392fdb327460a902dc78f8b6
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2021-12-11 21:19:42 +0100

    working clock

--- app/src/main/java/com/example/exampleclock/MainActivity.java
+++ app/src/main/java/com/example/exampleclock/MainActivity.java
@@ -1,19 +1,47 @@
 package com.example.exampleclock;
 
+import android.nfc.tech.TagTechnology;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.view.View;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Date date = new Date();
-        TextView displayDate = findViewById(R.id.TextViewDate);
-        displayDate.setText(date.toString());
+        this.textView = findViewById(R.id.TextViewDate);
+
+        refresh();
+    }
+    private void refresh() {
+        final Handler handler = new Handler();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(550);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(new SimpleDateFormat("HH:MM:ss").format(new Date()));
+                        refresh();
+                    }
+                });
+            }
+        }).start();
     }
 }
